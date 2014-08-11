6452f950356b90cb5258f006c79e85bf040dd7e5
Simándi Gergely <s.geri92@gmail.com>
2014-08-11 16:29:35 +0200

    Json
    +JsonSerializer

---
+++ JsonSerializer.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalExtensions
+{
+    public class JsonSerializer
+    {
+        public async static Task<T> Deserialize<T>(string json)
+        {
+            return await JsonConvert.DeserializeObjectAsync<T>(json);
+        }
+
+        public async static Task<string> Serialize<T>(List<T> list)
+        {
+            return await JsonConvert.SerializeObjectAsync(list);
+        }
+    }
+}
